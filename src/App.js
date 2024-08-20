4bafd65b4602554aa93db4397f62eacea36689b9
Ambati Niranjana Srinivasu <ambati.ns04@gmail.com>
2024-08-20 20:39:36 +0530

    added chakra ui

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 
+import { ChakraProvider } from '@chakra-ui/react';
 import './App.css';
 
 function App() {
   return (
+    <ChakraProvider>
     <div>
       
     </div>
+    </ChakraProvider>
   );
 }
 
